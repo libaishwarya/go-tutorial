961461f76c554c15ac9dad8786b8be17d4e82932
libaishwarya <libaishwarya@gmail.com>
2024-04-03 13:51:15 +0530

    map implementation with comma ok syntax

--- go-maps/main.go
+++ go-maps/main.go
@@ -9,7 +9,7 @@ import "fmt"
 func main() {
 	var m map[string]int //map keyword followed by key type(string)and value type(int)
 	fmt.Println(m)
-	m = map[string]int{"foo": 1, "bar": 2}
+	m = map[string]int{"foo": 1, "bar": 2, "sun": 32, "hen": 54}
 	fmt.Println(m)
 
 	fmt.Println(m["bar"]) //Accessing value in map
@@ -21,5 +21,16 @@ func main() {
 
 	delete(m, "bar") //deleting value
 	fmt.Println(m)
+
+	fmt.Println(m["bar"])
+	v, ok := m["bar"]  //
+	fmt.Println(v, ok) //key is not present in map so ok returns False and v returns 0 since not found
+	g, ok := m["foo"]
+	fmt.Println(g, ok)
+
+	m2 := m
+	m["foo"], m2["gun"], m["bar"] = 65, 83, 32
+	fmt.Println("m value is: ", m)
+	fmt.Println("m2 value is: ", m2)
 
 }
